1e6dd49e000d7b0c569049f90f4cc0141a93ae1f
argoday <argoday@argoday.com>
2016-05-13 16:17:55 -0400

    Move git install earlier

--- deploy/cloud-init.sh
+++ deploy/cloud-init.sh
@@ -1,3 +1,5 @@
 
 wget -qO- https://www.distelli.com/download/client | sh
 sudo /usr/local/bin/distelli agent install
+
+sudo apt-get install git
--- deploy/setup.sh
+++ deploy/setup.sh
@@ -3,8 +3,6 @@ sudo apt-get install software-properties-common -y
 sudo apt-add-repository ppa:ansible/ansible -y
 sudo apt-get update
 
-sudo apt-get install git
-
 sudo apt-get install awscli -y
 
 sudo apt-get install ansible -y
