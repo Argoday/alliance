1f5aa9c174df1292aa4c46f5c581bc6a83bc2450
argoday <argoday@argoday.com>
2016-05-19 23:28:08 -0400

    Rearrange code

--- app/Config.scala
+++ app/Config.scala
@@ -1,11 +1,4 @@
-import javax.inject._
-
-import play.api.http.DefaultHttpErrorHandler
-import play.api._
-import play.api.mvc._
-import play.api.mvc.Results._
-import play.api.routing.Router
-import scala.concurrent._
+import play.api.Configuration
 
 object Config {
   object Stage {
@@ -48,19 +41,3 @@ object Config {
   }
 }
 
-class ErrorHandler @Inject() (
-    env: Environment,
-    config: Configuration,
-    sourceMapper: OptionalSourceMapper,
-    router: Provider[Router]
-  ) extends DefaultHttpErrorHandler(env, config, sourceMapper, router) {
-
-  override def onNotFound(request: RequestHeader, message: String): Future[Result] = {
-    if (Config.isLocal) {
-      Future.successful(NotFound(views.html.defaultpages.devNotFound(request.method, request.uri, Option(router.get))))
-    } else {
-      Future.successful(NotFound(views.html.notFound()))
-    }
-  }
-
-}
--- app/Module.scala
+++ app/Module.scala
@@ -1,15 +1,36 @@
 import com.google.inject.AbstractModule
-import play.api.{ Configuration, Environment }
 import java.time.Clock
+import javax.inject._
+import scala.concurrent.Future
+
+import play.api.http.DefaultHttpErrorHandler
+import play.api.{ Configuration, Environment, OptionalSourceMapper }
+import play.api.mvc.{Result, RequestHeader}
+import play.api.mvc.Results.NotFound
+import play.api.routing.Router
 
 class Module(environment: Environment, configuration: Configuration) extends AbstractModule {
-
   override def configure() = {
     // Use the system clock as the default implementation of Clock
     bind(classOf[Clock]).toInstance(Clock.systemDefaultZone)
 
     Config.init(configuration)
+  }
+}
 
+class ErrorHandler @Inject() (
+    env: Environment,
+    config: Configuration,
+    sourceMapper: OptionalSourceMapper,
+    router: Provider[Router]
+  ) extends DefaultHttpErrorHandler(env, config, sourceMapper, router) {
+
+  override def onNotFound(request: RequestHeader, message: String): Future[Result] = {
+    if (Config.isLocal) {
+      Future.successful(NotFound(views.html.defaultpages.devNotFound(request.method, request.uri, Option(router.get))))
+    } else {
+      Future.successful(NotFound(views.html.notFound()))
+    }
   }
 
 }
